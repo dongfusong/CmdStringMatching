af4d488fac245a8de1d36511f92e1c3db81b6ab0
dongfusong <dongfusong@126.com>
2014-11-15 13:34:41 +0800

    start matcher project

---
+++ test/TestCmdMather.cpp
@@ -0,0 +1,25 @@
+/*
+ * TestCmdMather.cpp
+ *
+ *  Created on: 2014Äê11ÔÂ15ÈÕ
+ *      Author: Thoughtworks
+ */
+#include <gtest/gtest.h>
+class TestCmdMather: public testing::Test {
+public:
+	void SetUp() {
+
+	}
+	void TearDown() {
+
+	}
+protected:
+};
+
+TEST_F(TestCmdMather, )
+{
+
+}
+
+
+
